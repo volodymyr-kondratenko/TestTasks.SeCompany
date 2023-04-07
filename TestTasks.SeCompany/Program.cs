d1026cf412a8c1882312ad630cd9ef83376f2cd5
volodymyr.kondratenko <ikondratenkova@gmail.com>
2023-04-07 20:50:20 +0300

    Initial commit.

---
+++ TestTasks.SeCompany/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TestTasks.SeCompany
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
